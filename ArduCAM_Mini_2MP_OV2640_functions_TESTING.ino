969866955d3c8902e3e360e27810ae1eee29ae2e
lukarodrigues <118940055+lukarodrigues@users.noreply.github.com>
2022-11-27 16:54:12 -0500

    Add files via upload

---
+++ ArduCAM_Mini_2MP_OV2640_functions_TESTING.ino
@@ -0,0 +1,255 @@
+// ArduCAM Mini demo (C)2017 Lee
+// Web: http://www.ArduCAM.com
+// This program is a demo of how to use most of the functions
+// of the library with ArduCAM Mini camera, and can run on any Arduino platform.
+// This demo was made for ArduCAM_Mini_5MP_Plus.
+// It needs to be used in combination with PC software.
+// It can test OV2640 functions
+// This program requires the ArduCAM V4.0.0 (or later) library and ArduCAM_Mini_5MP_Plus
+// and use Arduino IDE 1.6.8 compiler or above
+
+
+
+#include <Wire.h>
+#include <ArduCAM.h>
+#include <SPI.h>
+#include "memorysaver.h"
+//This demo can only work on OV2640_MINI_2MP platform.
+#if !(defined OV2640_MINI_2MP)
+  #error Please select the hardware platform and camera module in the ../libraries/ArduCAM/memorysaver.h file
+#endif
+#define BMPIMAGEOFFSET 66
+
+
+// set pin 7 as the slave select for the digital pot:
+const int CS = 7;
+const int sensor = 6;
+bool is_header = false;
+int mode = 0;
+int state = LOW;
+uint8_t start_capture = 0;
+#if defined (OV2640_MINI_2MP)
+  ArduCAM myCAM( OV2640, CS );
+#else
+  ArduCAM myCAM( OV5642, CS );
+#endif
+uint8_t read_fifo_burst(ArduCAM myCAM);
+
+int sensorValue;
+
+
+
+void setup() {
+
+
+    pinMode(sensor, INPUT);    // initialize sensor as an input
+
+// put your setup code here, to run once:
+uint8_t vid, pid;
+uint8_t temp;
+#if defined(__SAM3X8E__)
+  Wire1.begin();
+  Serial.begin(115200);
+#else
+  Wire.begin();
+  Serial.begin(921600);
+#endif
+Serial.println(F("ACK CMD ArduCAM Start! END"));
+// set the CS as an output:
+pinMode(CS, OUTPUT);
+digitalWrite(CS, HIGH);
+// initialize SPI:
+SPI.begin();
+  //Reset the CPLD
+myCAM.write_reg(0x07, 0x80);
+delay(100);
+myCAM.write_reg(0x07, 0x00);
+delay(100);
+while(1){
+  //Check if the ArduCAM SPI bus is OK
+  myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
+  temp = myCAM.read_reg(ARDUCHIP_TEST1);
+  if (temp != 0x55){
+    Serial.println(F("ACK CMD SPI interface Error! END"));
+    delay(1000);continue;
+  }else{
+    Serial.println(F("ACK CMD SPI interface OK. END"));break;
+  }
+}
+
+#if defined (OV2640_MINI_2MP)
+  while(1){
+    //Check if the camera module type is OV2640
+    myCAM.wrSensorReg8_8(0xff, 0x01);
+    myCAM.rdSensorReg8_8(OV2640_CHIPID_HIGH, &vid);
+    myCAM.rdSensorReg8_8(OV2640_CHIPID_LOW, &pid);
+    if ((vid != 0x26 ) && (( pid != 0x41 ) || ( pid != 0x42 ))){
+      Serial.println(F("ACK CMD Can't find OV2640 module! END"));
+      delay(1000);continue;
+    }
+    else{
+      Serial.println(F("ACK CMD OV2640 detected. END"));break;
+    } 
+  }
+#else
+  while(1){
+    //Check if the camera module type is OV5642
+    myCAM.wrSensorReg16_8(0xff, 0x01);
+    myCAM.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
+    myCAM.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
+    if((vid != 0x56) || (pid != 0x42)){
+      Serial.println(F("ACK CMD Can't find OV5642 module! END"));
+      delay(1000);continue;
+    }
+    else{
+      Serial.println(F("ACK CMD OV5642 detected. END"));break;
+    } 
+  }
+#endif
+
+
+//Change to JPEG capture mode and initialize the OV5642 module
+myCAM.set_format(JPEG);
+myCAM.InitCAM();
+
+
+#if defined (OV2640_MINI_2MP)
+  myCAM.OV2640_set_JPEG_size(OV2640_320x240);
+#else
+  myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);   //VSYNC is active HIGH
+  myCAM.OV5642_set_JPEG_size(OV5642_320x240);
+#endif
+delay(1000);
+myCAM.clear_fifo_flag();
+#if !(defined (OV2640_MINI_2MP))
+myCAM.write_reg(ARDUCHIP_FRAMES,0x00);
+#endif
+}
+
+
+
+
+void loop() {
+// put your main code here, to run repeatedly:
+uint8_t temp = 0xff, temp_last = 0;
+bool is_header = false;
+
+if (Serial.available())//CHECK FOR SENSOR
+{
+  sensorValue = digitalRead(sensor);
+
+  temp = Serial.read();
+  
+
+  //set image size
+  myCAM.OV2640_set_JPEG_size(OV2640_320x240);
+  delay(1000);
+  Serial.println(F("ACK CMD switch to OV2640_320x240 END"));
+  temp = 0xff;
+
+
+
+
+  
+  if(sensorValue == LOW){
+    Serial.println("Motion detected!"); 
+    state = HIGH;       // update variable state to HIGH
+    delay(500);
+
+
+    mode = 1;//SET TO SINGLE CAPTURE
+    temp = 0xff;//IDK WHAT THIS DOES
+    start_capture = 1;
+    Serial.println(F("ACK CMD CAM start single shoot. END"));
+
+    myCAM.set_format(JPEG);
+    myCAM.InitCAM();
+    #if !(defined (OV2640_MINI_2MP))
+    myCAM.set_bit(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);
+    #endif
+
+
+    myCAM.OV2640_set_Light_Mode(Auto);temp = 0xff;
+    Serial.println(F("ACK CMD Set to Auto END"));
+
+
+  }
+  
+  else{
+
+  delay(500);
+    if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
+    }
+  }
+
+
+
+
+
+
+if (mode == 1)
+{
+  if (start_capture == 1)
+  {
+    myCAM.flush_fifo();
+    myCAM.clear_fifo_flag();
+    //Start capture
+    myCAM.start_capture();
+    start_capture = 0;
+  }
+  if (myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK))
+  {
+    Serial.println(F("ACK CMD CAM Capture Done. END"));delay(50);
+    read_fifo_burst(myCAM);
+    //Clear the capture done flag
+    myCAM.clear_fifo_flag();
+  }
+}
+}
+}
+uint8_t read_fifo_burst(ArduCAM myCAM)
+{
+  uint8_t temp = 0, temp_last = 0;
+  uint32_t length = 0;
+  length = myCAM.read_fifo_length();
+  Serial.println(length, DEC);
+  if (length >= MAX_FIFO_SIZE) //512 kb
+  {
+    Serial.println(F("ACK CMD Over size. END"));
+    return 0;
+  }
+  if (length == 0 ) //0 kb
+  {
+    Serial.println(F("ACK CMD Size is 0. END"));
+    return 0;
+  }
+  myCAM.CS_LOW();
+  myCAM.set_fifo_burst();//Set fifo burst mode
+  temp =  SPI.transfer(0x00);
+  length --;
+  while ( length-- )
+  {
+    temp_last = temp;
+    temp =  SPI.transfer(0x00);
+    if (is_header == true)
+    {
+      Serial.write(temp);
+    }
+    else if ((temp == 0xD8) & (temp_last == 0xFF))
+    {
+      is_header = true;
+      Serial.println(F("ACK IMG END"));
+      Serial.write(temp_last);
+      Serial.write(temp);
+    }
+    if ( (temp == 0xD9) && (temp_last == 0xFF) ) //If find the end ,break while,
+    break;
+    delayMicroseconds(15);
+  }
+  myCAM.CS_HIGH();
+  is_header = false;
+  return 1;
+}
+
